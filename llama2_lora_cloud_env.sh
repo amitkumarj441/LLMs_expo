1c683b3e63791fb3b37be42d72bd7c1cf97e32f8
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2023-12-01 23:58:21 +0000

    Create llama2_lora_cloud_env.sh

---
+++ llama2_lora_cloud_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+# for download llama2 models
+mkdir models
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+git lfs install
+git clone https://huggingface.co/meta-llama/Llama-2-70b-chat-hf
+
+# for test_lora.py
+export AZUREML_OAI_LLAMA_2_7B_HF_PATH="/models/Llama-2-70b-chat-hf"
+export AZUREML_OAI_LLAMA_2_7B_HF_PATH="/models/Llama-2-70b-chat-hf"
+export AZUREML_OAI_LLAMA_2_7B_HF_PATH="/sys/fs/cgroup/models/Llama-2-70b-chat-hf"
+export AZUREML_OAI_DISABLE_BILLING_EVENTS=0
