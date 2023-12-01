2fa5d9d408912c9df6fcd4d6010b46a2df7a7afa
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2023-12-01 23:59:32 +0000

    Update llama2_lora_cloud_env.sh

--- llama2_lora_cloud_env.sh
+++ llama2_lora_cloud_env.sh
@@ -9,7 +9,7 @@ git lfs install
 git clone https://huggingface.co/meta-llama/Llama-2-70b-chat-hf
 
 # for test_lora.py
-export AZUREML_OAI_LLAMA_2_7B_HF_PATH="/models/Llama-2-70b-chat-hf"
-export AZUREML_OAI_LLAMA_2_7B_HF_PATH="/models/Llama-2-70b-chat-hf"
-export AZUREML_OAI_LLAMA_2_7B_HF_PATH="/sys/fs/cgroup/models/Llama-2-70b-chat-hf"
+export AZUREML_OAI_LLAMA_2_70B_HF_PATH="/models/Llama-2-70b-chat-hf"
+export AZUREML_OAI_LLAMA_2_70B_HF_PATH="/models/Llama-2-70b-chat-hf"
+export AZUREML_OAI_LLAMA_2_70B_HF_PATH="/sys/fs/cgroup/models/Llama-2-70b-chat-hf"
 export AZUREML_OAI_DISABLE_BILLING_EVENTS=0
